b6c6425eb62d0a4e3231da63042fec2a0eb9b4b9
Raul Herrera <raulherrera01022003@gmail.com>
2023-11-12 23:44:17 -0500

    Versión del compilador modificada

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.uniagustiniana.proyecto_final_foro"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.uniagustiniana.proyecto_final_foro"
